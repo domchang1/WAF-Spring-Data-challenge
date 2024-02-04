74370e789df23efc4a24f8ca3a80f3e802e64621
domchang1 <dominicchang08@gmail.com>
2024-02-04 17:21:11 -0500

    starting clustering

--- Classification.py
+++ Classification.py
@@ -6,21 +6,32 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.ensemble import RandomForestClassifier
 
-
+#read in data
 dataset = pd.read_csv('combined_data.csv', delimiter=',', encoding='latin-1')
 
+#separate into inputs and outputs, remove results from inputs
 labels = (dataset[['won']]).values.tolist()
-inputs = dataset.drop(columns=['Unnamed: 0', 'won'])
+inputs = dataset.drop(columns=['Unnamed: 0', 'won', 'place_combination1', 'place_combination2'])
 labels = np.array(labels).squeeze()
 
+#split into 80-20 train test
 train_inputs, test_inputs, train_labels, test_labels = train_test_split(inputs, labels, test_size=0.2)
 
+#create, train, and predict using model
 model = RandomForestClassifier().fit(train_inputs, train_labels)
 predictions = model.predict(test_inputs)
 
-print(1- np.sum(np.abs((predictions-test_labels)) / len(test_labels)))
+#show results using confusion matrix
 cm = confusion_matrix(test_labels, predictions)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Did not win', 'Win'])
 disp.plot()
 plt.title('Model Predicting If Horse Wins')
 plt.show()
+
+#show feature importance
+importance = model.feature_importances_
+print(importance)
+print(inputs.columns) 
+
+#Accuracy, performance metric
+print("Accuracy: " + str(1- np.sum(np.abs((predictions-test_labels)) / len(test_labels))))
---
+++ Clustering.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+#UNFINISHED#
+
+#read in data
+dataset = pd.read_csv('combined_data.csv', delimiter=',', encoding='latin-1')
+dataset = dataset.drop(columns=['Unnamed: 0'])
+
+#Create/train clustering model
+model = KMeans(n_clusters=5).fit(dataset)
+
+# Get cluster labels and evaluate
+labels = model.labels_  
+silhouette_avg = silhouette_score(dataset, labels)
+print(f"Silhouette Score: {silhouette_avg}")
